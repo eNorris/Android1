915262e1e0f013e9f1ca2d37589ca26ede617a6b
Edward Norris <etnc6d@mst.edu>
2012-04-08 09:11:07 -0500

    Added a comment

--- Android1/src/k/android/project1/Android1Activity.java
+++ Android1/src/k/android/project1/Android1Activity.java
@@ -3,6 +3,11 @@ package k.android.project1;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * I just added this comment
+ * @author etnc6d
+ *
+ */
 public class Android1Activity extends Activity {
     /** Called when the activity is first created. */
     @Override
