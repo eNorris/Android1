1d23de0357932c00e20cd9530c2a8b70e8cc7a99
Edward Norris <etnc6d@mst.edu>
2012-04-08 09:01:59 -0500

    Created Eclipse Project

---
+++ Android1/src/k/android/project1/Android1Activity.java
@@ -0,0 +1,13 @@
+package k.android.project1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Android1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
